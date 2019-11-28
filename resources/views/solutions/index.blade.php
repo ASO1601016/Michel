0c77e4bf14ecdba98a0030de0d9088c370e45220
ASO1601013 <32320672+ASO1601013@users.noreply.github.com>
2019-11-28 11:29:54 +0900

    Update index.blade.php
    ボタン文字追加

--- resources/views/solutions/index.blade.php
+++ resources/views/solutions/index.blade.php
@@ -59,7 +59,7 @@
                                             <p class="card-title">{{$newItems[$j+$i]->title}}</p>
                                             <form method="get" width="100%" name="form{{$newItems[$j+$i]->id}}" action="detail">
                                                 <input type="hidden" name="id" value="{{$newItems[$j+$i]->id}}">
-                                                <button style="" class="btn btn-warning mx-auto d-block" href="javascript:form{{$newItems[$j+$i]->id}}.submit()">
+                                                <button class="btn btn-warning mx-auto d-block" href="javascript:form{{$newItems[$j+$i]->id}}.submit()">詳細へ
                                                 </button>
                                             </form>
                                         </div>
