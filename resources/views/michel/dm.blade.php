e78c695966111ee86d6207d3280156e8226f8942
ASO1601016 <1601016@st.asojuku.ac.jp>
2019-11-28 11:26:27 +0900

    no message

--- resources/views/michel/dm.blade.php
+++ resources/views/michel/dm.blade.php
@@ -88,7 +88,7 @@ $(document).ready(function(){
     </script>
     <script type="text/javascript">
         $(function(){
-            $('#sousin').click(function(){
+            $('[type="submit"]').click(function(){
                 $(this).prop('disabled',true);//ボタンを無効化する
                 $(this).closest('form').submit();//フォームを送信する
             });
