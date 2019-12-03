cc0abbfc205d3de9ce848ac3c5d5953edbad75c7
ASO1601016 <1601016@st.asojuku.ac.jp>
2019-12-03 17:10:49 +0900

    footer文字変更 dmList画像はみ出ないように変更

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,9 +23,7 @@
             @yield('header')
             <header class="sticky-top">
                 <h4 class="container-fluid text-center border p-2 m-0 bg-white text-orange">
-                    
                     MICHEL
-                    
                 </h4>
                 <div class="text-small gradient text-right text-white pr-1">
                     @yield('headerTitle')
@@ -36,8 +34,8 @@
             @yield('content')
         </div>
 
-        <div class="footer">
-            copyright 2019 akasoo.
+        <div class="footer text-center">
+            copyright 2019 michel.
         </div>
     </body>
 </html>
