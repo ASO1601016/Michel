154f1d3d583b1a502efdb11de1409f34edfec153
ASO1601013 <32320672+ASO1601013@users.noreply.github.com>
2019-12-05 13:01:23 +0900

    Update login.blade.php
    ログイン画面デザイン変更

--- resources/views/hello/login.blade.php
+++ resources/views/hello/login.blade.php
@@ -1,13 +1,13 @@
-@extends('layouts.guest')
+@extends('layouts.login')
 @section('css')
     <link rel="stylesheet" href="{{ asset('/assets/css/login.css') }}">
 @endsection
 
-@section('content')
-    <div class="text-center text-white p-1 mb-3" style="background-color:orange">
-        <strong>ログイン</strong>
-    </div>        
-        <div class="container-fluid">
+@section('content')     
+        <div class="container">
+            <img src="storage/image/titlebrk.png" alt="title">
+        </div>
+        <div class="container-fluid" style="margin-top:30%;">
             <div class="login-page">
                 <div class="form pw-form">
                     <form class="login-form pw-form-container" action="./login_check" method="post">
@@ -33,7 +33,7 @@
                         @if (($errors->has('id') || $errors->has('password')) && $flag == false)
                             <div class="bg-danger text-white mt-2 pl-1 border border-danger rounded ib"><b>{{ "学籍番号またはパスワードが違います。" }}</b></div>
                         @endif
-                        <input type="submit" value="ログイン" class="mt-4 border rounded border-danger p-2 mb-5 bg-white">
+                        <input type="submit" value="ログイン" class="mx-auto d-block rounded text-white mt-4 p-2" style="background-color:#F7B46B;border:none;font-weight:bold;">
                     </form>
                 </div>
             </div>
@@ -54,13 +54,10 @@
                 }
             });
         </script>
-        <div class="text-center text-white p-1 mb-3" style="background-color:orange">
-            <strong>新規会員登録</strong>
-        </div>
-        <div class="pl-2 mb-4" style="font-size: 75%;">まだ会員で無い方はこちら</div>
-        <div class="container">
+        <div class="pl-2 m-2" style="font-size: 75%;">まだ会員で無い方はこちら</div>
+        <div class="container-fluid">
             <div class="form">
-                <button onclick="location.href='./regist'" class="rounded  p-2 mb-5 bg-danger text-white p-1"><strong>新規会員登録を行う</strong></button>
+                <button onclick="location.href='./regist'" class="mx-auto d-block rounded text-white p-2 mb-5 w-100" style="background-color:#BCB5B5;border:none;"><strong>新規会員登録を行う</strong></button>
             </div>
         </div>
 @endsection
