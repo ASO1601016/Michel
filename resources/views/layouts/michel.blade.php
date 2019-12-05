e6988828df51f7ef13474151eeb015148806dabf
ASO1601013 <32320672+ASO1601013@users.noreply.github.com>
2019-12-05 13:27:39 +0900

    Update michel.blade.php
    headerのロゴ調整

--- resources/views/layouts/michel.blade.php
+++ resources/views/layouts/michel.blade.php
@@ -79,7 +79,7 @@
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid navcont">
             <div class="navbar-header d-block mx-auto">
-                <button class="titlebtn" onclick="location.href='./top'"><img src="storage/image/titlebrk.png"></button>
+                <button class="titlebtn" onclick="location.href='./top'"><img src="storage/image/titlebrk.png" width="25%"></button>
             </div>
         </div>     
     </nav>
