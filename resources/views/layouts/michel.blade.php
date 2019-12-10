61b3cbd04a7a65f89a4128642ac6201e7f6c209f
ASO1601013 <32320672+ASO1601013@users.noreply.github.com>
2019-12-10 10:36:14 +0900

    Update michel.blade.php
    よくわからんのコメントアウト〜〜！

--- resources/views/layouts/michel.blade.php
+++ resources/views/layouts/michel.blade.php
@@ -1,8 +1,9 @@
 <html>
     <head>
-        @php
+<!--        @php
             setcookie('hoge', 'fuga', time()+3600, '/; SameSite=Strict', '', true, true);
         @endphp
+-->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width,user-scalable=no">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
